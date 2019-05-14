e27c6a5c54c4c92fa93e3d92533b93363d17bd5f
Mike Metcalfe <mike@webtide.co.za>
2019-05-14 16:24:17 +0200

    Refactored multi coa to have methods and units grouped with AS title Added custom reportview to provide view method for mutli

--- src/bika/coa/interfaces.py
+++ src/bika/coa/interfaces.py
@@ -1,6 +1,8 @@
-from senaite.impress.interfaces import ISenaiteImpressLayer
+from bika.lims.interfaces import IBikaLIMS
+from senaite.impress.interfaces import ILayer as ISenaiteIMPRESS
+from senaite.lims.interfaces import ISenaiteLIMS
 
 
-class IBikaCOALayer(ISenaiteImpressLayer):
-    """Bika COA Browser Layer Interface
+class IBikaCOALayer(IBikaLIMS, ISenaiteLIMS, ISenaiteIMPRESS):
+    """Marker interface that defines a Zope 3 browser layer.
     """
--- src/bika/coa/reportview.py
+++ src/bika/coa/reportview.py
@@ -7,9 +7,20 @@ class MultiReportView(ReportView):
     """
 
     def __init__(self, collection, request):
-        import pdb; pdb.set_trace()
         logger.info("MultiReportView::__init__:collection={}"
                     .format(collection))
         super(MultiReportView, self).__init__(collection, request)
         self.collection = collection
         self.request = request
+
+    def get_common_row_data(self, collection, poc, category):
+        model = collection[0]
+        analyses = self.get_analyses_by(collection, poc=poc, category=category)
+        common_data = []
+        for analysis in analyses:
+            datum = [analysis.Title(), '-', model.get_formatted_unit(analysis)]
+            if analysis.Method:
+                datum[1] = analysis.Method.Title()
+            common_data.append(datum)
+        unique_data = self.uniquify_items(common_data)
+        return unique_data
