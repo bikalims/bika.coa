8292fa861ea9b76f6a68ed846adbe49ed915ca07
Lunga Baliwe <lunga001@gmail.com>
2020-03-05 01:18:51 +0200

    Add equine-multi report

--- src/bika/coa/reportview.py
+++ src/bika/coa/reportview.py
@@ -1,4 +1,7 @@
 from bika.coa import logger
+from bika.lims.workflow import getTransitionUsers
+from bika.lims import api
+from plone import api as ploneapi
 from senaite.impress.analysisrequest.reportview import MultiReportView as ReportView
 
 
@@ -24,3 +27,34 @@ class MultiReportView(ReportView):
             common_data.append(datum)
         unique_data = self.uniquify_items(common_data)
         return unique_data
+
+    def get_extra_data(self, collection=None, poc=None, category=None):
+        analyses = self.get_analyses(collection)
+        analyses = self.sort_items_by('DateSampled', analyses)
+        sampled_from = analyses[0].DateSampled
+        to = analyses[-1].DateSampled
+        analysis_title = analyses[-1].Title()
+        datum = {"methods": [], 'from': sampled_from, 'to': to,
+                 "analysis_title": analysis_title}
+        for analysis in analyses:
+            if analysis.Method:
+                method = '{} {}'.format(analysis.Method.Title(), analysis.Method.Description())
+                datum['methods'].append(method)
+        datum['methods'] = self.uniquify_items(datum['methods'])
+        return datum
+
+    def get_verifier(self, collection):
+        analyses = self.get_analyses_by(collection)
+        # TODO: Get transitioned user
+        actor = getTransitionUsers(analyses[0], 'verify')
+        user_name = actor[0] if actor else ''
+        user = api.get_user(user_name)
+        roles = ploneapi.user.get_roles(username=user_name)
+        return {"fullname": user.fullname, 'role': roles[0]}
+
+    def get_analyst(self, collection):
+        analyses = self.get_analyses_by(collection)
+        actor = getTransitionUsers(analyses[0], 'submit')
+        user = actor[0] if actor else ''
+        user = api.get_user(user)
+        return user.fullname
