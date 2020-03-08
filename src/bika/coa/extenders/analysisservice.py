58a1bcdd38199ffdebf17df5d101f858227010bd
Lunga Baliwe <lunga001@gmail.com>
2020-03-08 15:50:42 +0200

    Replace PickListWidget with BikaReferenceWidget because of error Module script, line 2, in displayValue    - <FSPythonScript at /senaite2/bika_setup/bika_analysisservices/analysisservice-3/displayValue>

--- src/bika/coa/extenders/analysisservice.py
+++ src/bika/coa/extenders/analysisservice.py
@@ -1,32 +1,11 @@
 from archetypes.schemaextender.interfaces import IOrderableSchemaExtender
 from archetypes.schemaextender.interfaces import ISchemaModifier
 from bika.lims import bikaMessageFactory as _
-from Products.Archetypes.atapi import DisplayList, PicklistWidget
-from Products.Archetypes.interfaces.vocabulary import IVocabulary
 from bika.lims.fields import ExtReferenceField
 from bika.lims.interfaces import IAnalysisService
 from zope.component import adapts
 from zope.interface import implements
 from bika.lims.browser.widgets import ReferenceWidget as BikaReferenceWidget
-from plone import api
-from Products.CMFCore.utils import getToolByName
-
-
-class Vocabulary_AnalysisService(object):
-    implements(IVocabulary)
-
-    def getDisplayList(self, context):
-        portal = api.portal.get()
-        bsc = getToolByName(portal, 'bika_setup_catalog')
-        analysisservices = bsc(portal_type='AnalysisService', sort_on='sortable_title')
-        items = [['', ''], ]
-        current_analysisservice = context.Title()
-        for analysisservice in analysisservices:
-            if current_analysisservice == analysisservice.Title:
-                continue
-            items.append([analysisservice.UID, analysisservice.Title])
-
-        return DisplayList(items)
 
 
 class AnalysisServiceSchemaExtender(object):
@@ -57,14 +36,26 @@ class AnalysisServiceSchemaExtender(object):
         ExtReferenceField(
             'AnalysisService',
             schemata='Method',
+            multiValued=1,
             allowed_types=['AnalysisService'],
             relationship='AnalysisServiceAnalysisService',
-            widget=PicklistWidget(
-                size=10,
-                label=_("Associated Analyses"),
+            widget=BikaReferenceWidget(
+                label=_("Associated analysisservices"),
+                size=20,
+                showOn=True,
+                search_fields=('Title'),
+                colModel=[
+                    {'columnName': 'Title',
+                     'width': '25', 'label': _('Title'),
+                     'align': 'left'},
+                    {'columnName': 'Keyword',
+                     'width': '25', 'label': _('Keyword'),
+                     'align': 'left'},
+                    # UID is required in colModel
+                    {'columnName': 'UID', 'hidden': True},
+                ],
             ),
         ),
-
     ]
 
     def __init__(self, context):
@@ -89,7 +80,6 @@ class AnalysisServiceSchemaModifier(object):
     def fiddle(self, schema):
         """
         """
-        schema['AnalysisService'].vocabulary = Vocabulary_AnalysisService()
         schema.moveField("AnalysisService", after="Methods")
         schema.moveField("Supplier", before="ManualEntryOfResults")
         return schema
