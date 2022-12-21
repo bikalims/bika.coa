fc893e8d8730c703fc06c2556003bb26d322846c
luthando5 <luthando@bikalabs.com>
2022-12-21 18:15:48 +0200

    Adding CSV update

--- src/bika/coa/ajax.py
+++ src/bika/coa/ajax.py
@@ -72,7 +72,10 @@ class AjaxPublishView(AP):
 
         csv_reports = []
         is_multi_template = self.is_multi_template(template)
-        if is_multi_template:
+        if template == "bika.coa:MultiBatch.pt":
+            csv_report= self.create_batch_csv_reports(samples,coa_num)
+            csv_reports = [csv_report for i in range(len(pdf_reports))]
+        elif is_multi_template:
             csv_report = self.create_csv_reports(samples)
             csv_reports = [csv_report for i in range(len(pdf_reports))]
         else:
@@ -119,6 +122,130 @@ class AjaxPublishView(AP):
             return api.get_url(self.context)
 
         return exit_urls[0]
+
+    def create_batch_csv_reports(self,samples,coa_num):
+        analyses = []
+        output = StringIO.StringIO()
+        top_headers= ["Unique COA ID","Project Title","Batch ID","Client","Date Sampled","Date Reported"]
+        group_cats = {}
+
+        # The key is the is the analysis service followed space to enter the results of the analysis service
+        # for the sample in question
+        field_analyses = {}
+        lab_analyses = {}
+        analysis_services = api.get_setup().bika_analysisservices.values()
+        for AS in analysis_services:
+            if AS.getPointOfCapture()=="field":
+                if AS.Title() not in field_analyses.keys():
+                    field_analyses[AS.Title()] = [""]*len(samples)
+            else:
+                if AS.Title() not in lab_analyses.keys():
+                    lab_analyses[AS.Title()] = [""]*len(samples)
+
+        for sample in samples:
+            date_received = sample.DateReceived
+            date_sampled = sample.DateSampled
+            date_reported = DateTime().Date()
+            coa_id =coa_num
+            batch_title = ""
+            if date_sampled:
+                date_sampled = date_sampled.strftime('%m-%d-%y')
+            if date_received:
+                date_received = date_received.strftime('%m-%d-%y')
+            date_verified = sample.getDateVerified()
+            if date_verified:
+                date_verified = date_verified.strftime('%m-%d-%y')
+            if sample.Batch:
+                batch_title = sample.Batch.title
+
+            writer = csv.writer(output)
+
+            headers_line= [
+                coa_id,
+                batch_title,
+                sample.BatchID,
+                sample.Client.title,
+                date_sampled,
+                date_reported
+            ]
+
+            analyses = sample.getAnalyses(full_objects=True)
+            for analysis in analyses:
+                analysis_info = {'sampleID':sample.id,
+                                 'samplePoint':sample.SamplePointTitle,
+                                 'sampleType':sample.SampleTypeTitle,
+                                 'title': analysis.Title(),
+                                 'result': analysis.getFormattedResult(html=False),
+                                 'unit': analysis.getService().getUnit(),
+                                 'obj':analysis,
+                                 'order':analysis.getSortKey()}
+                if analysis.getPointOfCapture() not in group_cats.keys():
+                    group_cats[analysis.getPointOfCapture()] = []
+                group_cats[analysis.getPointOfCapture()].append(analysis_info)
+
+            if len(headers_line) != len(top_headers):
+                for i in top_headers[len(headers_line):]:
+                    headers_line.append('')
+
+        sample_data, final_field_analyses, final_lab_analyses = self.create_sample_rows(group_cats,field_analyses,lab_analyses)
+        header_rows = self.merge_header_and_values(top_headers,headers_line)
+
+        for row in header_rows:
+            writer.writerow(row)
+
+        for new_row in sample_data:
+            writer.writerow(new_row)
+        writer.writerow(["Field Analyses"])
+        for analyses_row in final_field_analyses.items():
+            results_row = analyses_row[1]
+            results_row.insert(0,analyses_row[0])
+            writer.writerow(results_row)
+
+        writer.writerow(["Lab Analyses"])
+        for lab_analyses_row in final_lab_analyses.items():
+            lab_results_row = lab_analyses_row[1]
+            lab_results_row.insert(0,lab_analyses_row[0])
+            writer.writerow(lab_results_row)
+
+        return output.getvalue()
+
+    def create_sample_rows(self,grouped_analyses,field_analyses,lab_analyses):
+        sample_ids = ["Sample ID"]
+        sample_Points = ["Sample Points"]
+        sample_Types = ["Sample Types"]
+
+        for Analysis_service in grouped_analyses.get("field"):
+            if Analysis_service.get("sampleID") not in sample_ids:
+                sample_ids.append(Analysis_service.get("sampleID"))
+                sample_Points.append(Analysis_service.get("samplePoint"))
+                sample_Types.append(Analysis_service.get("sampleType"))
+            position_at_top = sample_ids.index(Analysis_service.get("sampleID")) - 1
+            title = Analysis_service.get('title')
+            field_analyses[title][position_at_top] = Analysis_service.get("result")
+        
+        for lab_Analysis_service in grouped_analyses.get("lab"):
+            if lab_Analysis_service.get("sampleID") not in sample_ids:
+                sample_ids.append(lab_Analysis_service.get("sampleID"))
+                sample_Points.append(lab_Analysis_service.get("samplePoint"))
+                sample_Types.append(lab_Analysis_service.get("sampleType"))
+            position_at_top = sample_ids.index(lab_Analysis_service.get("sampleID")) - 1
+            title = lab_Analysis_service.get('title')
+            lab_analyses[title][position_at_top] = lab_Analysis_service.get("result")
+        sample_headers = [sample_ids,sample_Points,sample_Types]
+        return sample_headers,field_analyses,lab_analyses
+
+    def merge_header_and_values(self,headers,values):
+        """Merge the headers and their values to make writing to CSV easier"""
+        row1 = []
+        row2 = []
+        for num in range(len(headers)):
+            if num < len(headers)//2:
+                row1.append(headers[num])
+                row1.append(values[num])
+            else:
+                row2.append(headers[num])
+                row2.append(values[num])
+        return [row1,row2]
 
     def create_csv_report(self, sample):
         analyses = []
