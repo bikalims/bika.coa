d5635b140b214be87666b1b6ff0e2e8fc3c4e103
Lunga Baliwe <lunga001@gmail.com>
2023-01-31 07:41:18 +0200

    Also display templates from the testit.coa

--- src/bika/coa/ajax.py
+++ src/bika/coa/ajax.py
@@ -373,4 +373,4 @@ class AjaxPublishView(AP):
         """Returns the available templates
         """
         templates = api.get_registry_record("senaite.impress.templates")
-        return sorted([item for item in templates if 'bika' in item])
+        return sorted([item for item in templates if 'bika' in item or 'testit' in item])
