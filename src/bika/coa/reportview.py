54f59df692e12101f79e7719406cea1da9825926
Luthando <luthando@bikalabs.com>
2023-04-06 13:30:00 +0200

    Some background commented out code and reference definition checks

--- src/bika/coa/reportview.py
+++ src/bika/coa/reportview.py
@@ -453,9 +453,10 @@ class MultiReportView(MRV):
         for sample in samples:
             qcs = sample.getQCAnalyses(['verified', 'published'])
             for qc in qcs:
-                title = qc.getReferenceDefinition().Title()
-                if title not in titles:
-                    titles.append(title)
+                if qc.getReferenceDefinition():
+                    title = qc.getReferenceDefinition().Title()
+                    if title not in titles:
+                        titles.append(title)
         for Title in titles:
             final_titles = final_titles + ", " + Title
         return final_titles
