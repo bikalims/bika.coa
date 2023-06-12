68fb7bd5dc783d5df8d76b033ff297f5486704ec
Lunga Baliwe <lunga001@gmail.com>
2023-06-12 16:36:06 +0200

    Sort on get_templates and not resources

--- src/bika/coa/browser/utils.py
+++ src/bika/coa/browser/utils.py
@@ -1,21 +1,27 @@
 # -*- coding: utf-8 -*-
 
-from operator import attrgetter
-from plone.resource.utils import iterDirectoriesOfType
+import os
 from senaite.impress.template import TemplateFinder as TF
 
 
 class TemplateFinder(TF):
 
-    @property
-    def resources(self):
-        out = []
-        directories = iterDirectoriesOfType(self.type)
-        sorted_directories = sorted(directories, key=attrgetter('__name__'))
-        for resource in sorted_directories:
-            out.append({
-                "name": resource.__name__,
-                "path": resource.directory,
-                "contents": resource.listDirectory(),
-            })
-        return out
+    def get_templates(self, extensions=[".pt", ".html"]):
+        templates = []
+        for resource in self.resources:
+            name = resource["name"]
+            path = resource["path"]
+            contents = resource["contents"] or []
+            for content in contents:
+                basename, ext = os.path.splitext(content)
+                if ext not in extensions:
+                    continue
+                if basename.lower().startswith("example"):
+                    continue
+                template = content
+                if name:
+                    template = u"{}:{}".format(name, content)
+                template_path = os.path.join(path, content)
+                templates.append((template, template_path))
+        templates.sort(key=lambda x: x[0])
+        return templates
