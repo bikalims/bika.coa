98f98e8056246a8df693cb83d598b8a4fc2c49f7
DanE417 <daanyaaleffendi1997@gmail.com>
2025-01-22 13:54:24 +0200

    Added model to concrete

--- src/bika/coa/reportview.py
+++ src/bika/coa/reportview.py
@@ -371,8 +371,8 @@ class SingleReportView(SRV):
             return batch.get_mix_design()
         return
 
-    def get_mix_design_concrete(self):
-        mix_design = self.get_mix_design()
+    def get_mix_design_concrete(self, model):
+        mix_design = self.get_mix_design(model)
         if not mix_design:
             return None
         return mix_design.get_mix_design_concrete()
