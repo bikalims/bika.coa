3fa28ecf35fc637e97ba23d1f986c70d15ee74c5
Luthando Ndlela <luthando.ndlela1@gmail.com>
2025-01-27 09:59:04 +0200

    Add row data method for mix materials listing

--- src/bika/coa/reportview.py
+++ src/bika/coa/reportview.py
@@ -397,13 +397,12 @@ class SingleReportView(SRV):
         mix_type_obj = api.get_object_by_uid(mix_type)
         return mix_type_obj.title
 
-    def get_mix_materials(self, model):
+    def get_mix_material_amounts(self, model):
         uids = self.get_mix_material_amount_uids(model)
         if not uids:
             return
         mix_material_amount_objs = [api.get_object_by_uid(x) for x in uids]
-        mix_material_objs = [api.get_object_by_uid(x.mix_material) for x in mix_material_amount_objs]
-        return mix_material_objs
+        return mix_material_amount_objs
 
     def get_mix_material_amount_uids(self, model):
         mix_design = self.get_mix_design(model)
@@ -413,6 +412,33 @@ class SingleReportView(SRV):
         if mix_material_amount_uids:
             return mix_material_amount_uids
         return
+
+    def get_mix_material_row_data(self, model):
+        row_data = []
+        mix_material_amount_objs = self.get_mix_material_amounts(model)
+        mix_material_objs = [api.get_object_by_uid(x.mix_material) for x in mix_material_amount_objs]
+        for row_num, mix_mat_amount in enumerate(mix_material_amount_objs):
+            mat_type_uid = mix_material_objs[row_num].material_type
+            if mat_type_uid:
+                mat_type = api.get_object_by_uid(mat_type_uid[0])
+            else:
+                mat_type = ""
+
+            mat_class = ""
+            if mat_type:
+                mat_class_uid = mat_type.material_class
+                if mat_class_uid:
+                    mat_class = api.get_object_by_uid(mat_class_uid[0]).title
+
+            if mat_type:
+                mat_type = mat_type.title
+
+            title = mix_material_objs[row_num].title
+            specific_gravity = mix_material_objs[row_num].specific_gravity
+            amount = mix_mat_amount.amounts
+            row_data.append([mat_class, mat_type, title, specific_gravity, amount])
+        return row_data
+
 
 class MultiReportView(MRV):
     """View for Bika COA Multi Reports
