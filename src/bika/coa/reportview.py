112457e18833a656f4992e66c0e6e914f4b8c7c4
Luthando Ndlela <luthando.ndlela1@gmail.com>
2025-01-27 15:53:15 +0200

    hyperlink titles in mix material listing table

--- src/bika/coa/reportview.py
+++ src/bika/coa/reportview.py
@@ -452,25 +452,29 @@ class SingleReportView(SRV):
         mix_material_amount_objs = self.get_mix_material_amounts(model)
         mix_material_objs = [api.get_object_by_uid(x.mix_material) for x in mix_material_amount_objs]
         for row_num, mix_mat_amount in enumerate(mix_material_amount_objs):
+
             mat_type_uid = mix_material_objs[row_num].material_type
+            mat_type = ""
             if mat_type_uid:
-                mat_type = api.get_object_by_uid(mat_type_uid[0])
-            else:
-                mat_type = ""
+                mat_type_obj = api.get_object_by_uid(mat_type_uid[0])
+                mat_type_url = mat_type_obj.absolute_url()
+                mat_type = [mat_type_obj.title, mat_type_url]
 
             mat_class = ""
-            if mat_type:
-                mat_class_uid = mat_type.material_class
+            if mat_type_obj:
+                mat_class_uid = mat_type_obj.material_class
                 if mat_class_uid:
-                    mat_class = api.get_object_by_uid(mat_class_uid[0]).title
-
-            if mat_type:
-                mat_type = mat_type.title
+                    mat_class_obj = api.get_object_by_uid(mat_class_uid[0])
+                    mat_class_url = mat_class_obj.absolute_url()
+                    mat_class = [mat_class_obj.title, mat_class_url]
 
             title = mix_material_objs[row_num].title
+            mix_material_url = mix_material_objs[row_num].absolute_url()
+            mix_material_title = [title, mix_material_url]
+
             specific_gravity = mix_material_objs[row_num].specific_gravity
             amount = mix_mat_amount.amounts
-            row_data.append([mat_class, mat_type, title, specific_gravity, amount])
+            row_data.append([mat_class, mat_type, mix_material_title, specific_gravity, amount])
         return row_data
 
 
