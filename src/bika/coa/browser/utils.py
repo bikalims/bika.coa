520fe79616cf46f8af39e11f28d5ee11e8362bf9
Luthando Ndlela <luthando.ndlela1@gmail.com>
2025-03-13 14:04:48 +0200

    Remove last header table

--- src/bika/coa/browser/utils.py
+++ src/bika/coa/browser/utils.py
@@ -11,7 +11,6 @@ from bika.coa import logger
 
 
 class TemplateFinder(TF):
-
     def get_templates(self, extensions=[".pt", ".html"]):
         templates = []
         for resource in self.resources:
@@ -34,8 +33,7 @@ class TemplateFinder(TF):
 
 
 class Publisher(Pu):
-    """Publishes HTML into printable formats
-    """
+    """Publishes HTML into printable formats"""
 
     def _layout_and_paginate(self, html):
         """Layout and paginate the given HTML into WeasyPrint `Document` objects
@@ -49,32 +47,38 @@ class Publisher(Pu):
         start = time.time()
         # Lay out and paginate the document
         html = HTML(
-            string=html, url_fetcher=self.url_fetcher, base_url=self.base_url)
+            string=html, url_fetcher=self.url_fetcher, base_url=self.base_url
+        )
         document = html.render(stylesheets=self.css)
         end = time.time()
-        logger.info("Publisher::Layout step took {:.2f}s for {} pages"
-                    .format(end-start, len(document.pages)))
+        logger.info(
+            "Publisher::Layout step took {:.2f}s for {} pages".format(
+                end - start, len(document.pages)
+            )
+        )
         number_of_pages = str(len(document.pages))
         del html
         del document
         # NOTE: Rerun now that we have number_of_pages HACK!!!
-        tags = old_html.find_all("div", text=lambda x: x and "number_of_pages" in x)
+        tags = old_html.find_all(
+            "div", text=lambda x: x and "number_of_pages" in x
+        )
         for tag in tags:
             tag.string = tag.text.replace("number_of_pages", number_of_pages)
 
         has_remarks_table = False
-        tag = old_html.find("div", text=lambda x: x and "remarks_table_paging" in x)
+        tag = old_html.find("div", class_=lambda x: x and "remarks-block" in x)
         if tag:
-            tag.string = tag.text.replace("remarks_table_paging", number_of_pages)
             has_remarks_table = True
 
         tag = old_html.find("div", text=lambda x: x and "last" in x)
         if tag:
             if has_remarks_table:
-                tag.string = tag.text.replace("last", str(int(number_of_pages) - 1))
+                tag.string = tag.text.replace(
+                    "last", str(int(number_of_pages) - 1)
+                )
             else:
                 tag.string = tag.text.replace("last", number_of_pages)
-
 
         # ensure we have plain html and not a BS4 node
         html = self.to_html(old_html)
@@ -82,9 +86,13 @@ class Publisher(Pu):
         start = time.time()
         # Lay out and paginate the document
         html = HTML(
-            string=html, url_fetcher=self.url_fetcher, base_url=self.base_url)
+            string=html, url_fetcher=self.url_fetcher, base_url=self.base_url
+        )
         document = html.render(stylesheets=self.css)
         end = time.time()
-        logger.info("Publisher::Layout step took {:.2f}s for {} pages"
-                    .format(end-start, len(document.pages)))
+        logger.info(
+            "Publisher::Layout step took {:.2f}s for {} pages".format(
+                end - start, len(document.pages)
+            )
+        )
         return document
